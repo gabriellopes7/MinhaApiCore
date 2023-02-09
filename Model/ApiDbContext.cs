19bf3b719079e5fd2f1e7a2d57d1385db98bc75c
Gabriel Ladeira <gabriellopes7@yahoo.com>
2023-02-09 17:15:00 -0300

    First Commit

---
+++ Model/ApiDbContext.cs
@@ -0,0 +1,15 @@
+using Microsoft.EntityFrameworkCore;
+
+namespace MinhaApiCore.Model
+{
+    public class ApiDbContext : DbContext
+    {
+        public ApiDbContext(DbContextOptions options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Fornecedor> Fornecedores {get;set;}
+        
+    }
+}
---
+++ Model/Fornecedor.cs
@@ -0,0 +1,23 @@
+using System.ComponentModel.DataAnnotations;
+
+namespace MinhaApiCore.Model
+{
+    public class Fornecedor
+    {
+        [Key]
+        public Guid Id { get; set; }
+
+        [Required]
+        [StringLength(100, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 2)]
+        public string Nome { get; set; }
+
+        [Required]
+        [StringLength(14, ErrorMessage = "O campo {0} precisa ter entre {2} e {1} caracteres", MinimumLength = 11)]
+        public string Documento { get; set; }
+
+        public int TipoFornecedor { get; set; }
+
+        public bool Ativo { get; set; }
+
+    }
+}
